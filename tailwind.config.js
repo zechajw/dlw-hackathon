0d5c829391ef9abbab21330d946b529f587a8d6b
Rayner Loh <raynerljm@gmail.com>
2021-10-16 13:02:27 +0800

    Initalize favicon, colours, font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: "'Lato', 'sans-serif'",
+      },
+      colors: {
+        ai: {
+          DEFAULT: "#E81F68",
+          50: "#FDEFF4",
+          100: "#FBD8E5",
+          200: "#F6AAC6",
+          300: "#F17CA6",
+          400: "#ED4D87",
+          500: "#E81F68",
+          600: "#C01452",
+          700: "#920F3F",
+          800: "#640A2B",
+          900: "#360517",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
