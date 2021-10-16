a55a9bfc1e05c3799bc2752f6eae7ad08bb428d6
zechajw <e0530207@u.nus.edu>
2021-10-16 22:38:54 +0800

    Change the steps to columns

--- components/Analyzer.tsx
+++ components/Analyzer.tsx
@@ -22,10 +22,10 @@ const Analyzer: FC = () => {
       <h2 className="mt-12 text-3xl font-bold text-white">
         How to export your Telegram chat history
       </h2>
-      <ol className="mt-8 text-xl text-center text-white list-decimal space-y-4">
-        <li>
-          <h3 className="text-left">
-            Navigate to the personal chat that you would like to analyze on{" "}
+      <div className="grid grid-cols-4 gap-x-12 mt-5 text-xl text-white justify-between">
+        <div>
+          <h3 className="text-justify">
+            1. Navigate to the personal chat that you would like to analyze on{" "}
             <a
               className="text-blue-400 underline cursor-pointer"
               href="https://desktop.telegram.org/"
@@ -35,11 +35,11 @@ const Analyzer: FC = () => {
               Telegram Desktop
             </a>
           </h3>
-        </li>
-        <li>
-          <h3 className="mb-4 text-left">
-            Click the triple dot button on the top right and then click 'Export
-            chat history'
+        </div>
+        <div className="flex flex-col items-center">
+          <h3 className="mb-4 text-justify">
+            2. Click the triple dot button on the top right and then click
+            'Export chat history'
           </h3>
           <Image
             src="/images/export-chat-history-1.png"
@@ -49,11 +49,11 @@ const Analyzer: FC = () => {
             className="rounded-2xl"
             layout="fixed"
           />
-        </li>
-        <li>
-          <h3 className="mb-4 text-left">
-            Uncheck all boxes, select format as 'JSON', and select a time period
-            ranging from 3 days to 7 days
+        </div>
+        <div className="flex flex-col items-center">
+          <h3 className="mb-4 text-justify">
+            3. Uncheck all boxes, select format as 'JSON', and select a time
+            period ranging from 3 days to 7 days
           </h3>
           <Image
             src="/images/export-chat-history-2.png"
@@ -63,11 +63,11 @@ const Analyzer: FC = () => {
             className="rounded-2xl"
             layout="fixed"
           />
-        </li>
-        <li>
-          <h3 className="mb-4 text-left">Click the 'Export' button</h3>
-        </li>
-      </ol>
+        </div>
+        <div>
+          <h3 className="mb-4 text-justify">4. Click the 'Export' button</h3>
+        </div>
+      </div>
       <div id="try-now" className="h-screen grid place-items-center">
         <div>
           <h1 className="text-5xl font-bold text-center text-white">
