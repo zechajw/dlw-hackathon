4ff8dbb6811480c55e8feb92671837c6cb01af57
Rayner Loh <raynerljm@gmail.com>
2021-10-16 15:33:06 +0800

    navbar stuff

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 const AboutUs: FC = () => {
   return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,31 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import NavItem from "./NavItem";
-import Image from "next/image";
-import Logo from "../public/icons/white-nobg.png";
 import Button from "./Button";
 
 const Navbar: FC = () => {
+  const [blackBG, setBlackBG] = useState(false);
+
+  useEffect(() => {
+    // Button is displayed after scrolling for 50 pixels
+    const toggleVisibility = (): void => {
+      if (window.pageYOffset > window.innerHeight) {
+        setBlackBG(true);
+      } else {
+        setBlackBG(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
-    <div className="fixed flex flex-row items-center w-full h-auto px-8 py-6 bg-transparent">
+    <div
+      className={`fixed flex flex-row items-center w-full h-auto px-8 py-6 ${
+        blackBG ? "bg-svg" : "bg-transparent"
+      }`}
+    >
       <div>
         <a
           className="text-4xl font-black text-white cursor-pointer bg-ai hover:text-transparent bg-clip-text"
