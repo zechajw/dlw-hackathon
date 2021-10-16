6df8ca1f9811853cb5e6c74c874baea115b3ab42
Rayner Loh <raynerljm@gmail.com>
2021-10-16 14:11:02 +0800

    add loading page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import Loading from "../components/Loading";
 import Body from "../components/Body";
 import LandingPage from "../components/LandingPage";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Loading />
       <Body>
         <LandingPage />
       </Body>
